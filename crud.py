8298c02f74d2c614bd997a0ccaf2bab96bf45158
sky7214sky72 <sky7214sky72@naver.com>
2022-07-04 18:44:03 +0900

    fastapi

--- crud.py
+++ crud.py
@@ -1,7 +1,12 @@
 from sqlalchemy.orm import Session
-
+from datetime import datetime
 import models
+import schemas
 
 
 def get_weather(db: Session):
     return db.query(models.Weather).all()
+
+
+def get_weather_detail(db: Session, weather_id: int):
+    return db.query(models.Weather).filter(models.Weather.id == weather_id).first()
--- main.py
+++ main.py
@@ -24,5 +24,12 @@ def get_db():
 
 @app.get("/", response_model=List[schemas.Weather])
 def read_weather(db: Session = Depends(get_db)):
-    weather = crud.get_weather(db)
+    weathers = crud.get_weather(db)
+    return weathers
+
+
+@app.get("/weather/{weather_id}", response_model=schemas.Weather)
+def read_weather_detail(weather_id: int, db: Session = Depends(get_db)):
+    weather = crud.get_weather_detail(db, weather_id)
     return weather
+
