7048d1028db38b5555d40af5e49e5024a22ee55c
sky7214sky72 <sky7214sky72@naver.com>
2022-07-04 15:48:15 +0900

    비밀

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "mysql+pymysql://logispot:logispot@localhost:3306/logispot_dev"
+SQLALCHEMY_DATABASE_URL = "mysql+pymysql://???:???@localhost:3306/???"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL
